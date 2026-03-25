538e474284ac598a6bf01c289eefd22012bfcfd1
dev0jha <ojha6773@gmail.com>
2026-03-25 15:48:43 +0530

    refactor: add job dispatch scheduler initialization

---
+++ scheduler.ts
@@ -0,0 +1,15 @@
+import { jobDispatchScheduler } from "./queues/queues.ts";
+import { jobDispatchWorker } from "./queues/worker.ts";
+
+async function init() {
+  await Promise.all([
+    jobDispatchScheduler.upsertJobScheduler("job_dispatcher-schedulers", {
+      every: 2 * 1000,
+    }),
+  ]);
+}
+
+init().catch((error) => {
+  console.error("[Scheduler]: Failed to initialize:", error);
+  process.exit(1);
+});
