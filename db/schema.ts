c56ef9c926be66ee01667894a260e9395fd59e1f
dev0jha <ojha6773@gmail.com>
2026-03-25 14:11:31 +0530

    refactor: add job status enum and state column to jobs table

--- db/schema.ts
+++ db/schema.ts
@@ -1,9 +1,17 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
+export const jobStatusEnum = pgEnum("job_status_enum", [
+  "SUBMITTED",
+  "RUNNABLE",
+  "RUNNING",
+  "SUCCEEDED",
+  "FAILED",
+]);
 export const jobsTable = pgTable("job", {
   id: uuid().primaryKey().defaultRandom(),
   image: text().notNull(),
   cmd: text(),
+  state: jobStatusEnum().default("SUBMITTED").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
